851322b1f15db655a8d751b0d2884be2392b6626
maximilienviallon <42693169+maximilienviallon@users.noreply.github.com>
2019-08-21 21:43:28 +0200

    added MineFieldElement Class

---
+++ MineSweeper/MineSweeper/MineFieldElement.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeper
+{
+    class MineFieldElement
+    {
+        private int id;
+        private int noOfNeighborMines;
+        private bool hasAMine = false;
+
+        public MineFieldElement(int pId)
+        {
+            this.id = pId;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public bool HasAMine
+        {
+            get
+            {
+                return hasAMine;
+            }
+
+            set
+            {
+                hasAMine = value;
+            }
+        }
+
+        public int NoOfNeighborMines
+        {
+            get
+            {
+                return noOfNeighborMines;
+            }
+
+            set
+            {
+                noOfNeighborMines = value;
+            }
+        }
+    }
+}
