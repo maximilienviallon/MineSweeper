e8f4cb1b488394348f526325bf5cdc39230e7f9d
2nowfake <baba_220@hotmail.com>
2019-08-30 04:09:41 +0200

    Added options for button

--- MineSweeper/MineSweeper/Form1.cs
+++ MineSweeper/MineSweeper/Form1.cs
@@ -12,9 +12,21 @@ namespace MineSweeper
 {
     public partial class Form1 : Form
     {
+        private Button btnAdd = new Button();
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            this.Controls.Add(btnAdd);
+            this.btnAdd.Location = new System.Drawing.Point(90, 25);
+
+            this.btnAdd.BackColor = Color.Gray;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.Size = new System.Drawing.Size(50, 25);
         }
     }
 }
