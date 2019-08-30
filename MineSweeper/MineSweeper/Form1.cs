d13a500f2b4872276faa9b43a363f673b3486833
2nowfake <baba_220@hotmail.com>
2019-08-30 06:09:03 +0200

    WORKS FOR NOW WITH IDENTIFYING BUTTONS

--- MineSweeper/MineSweeper/Form1.cs
+++ MineSweeper/MineSweeper/Form1.cs
@@ -12,7 +12,7 @@ namespace MineSweeper
 {
     public partial class Form1 : Form
     {
-        public static Button gButtom { get; }
+        //public static Button gButtom { get; }
 
         //private Button btnAdd2 = new Button();
         public Form1()
@@ -22,18 +22,20 @@ namespace MineSweeper
             {
                 Button gButtom = new Button();
                 Controls.Add(gButtom);
-                gButtom.Location = new System.Drawing.Point(90+25*i, 25);
+                gButtom.Location = new System.Drawing.Point(90+i*25, 25);
                 gButtom.Size = new System.Drawing.Size(25, 25);
                 gButtom.BackColor = Color.LightGray;
-            
+                gButtom.Click += new System.EventHandler(this.Button_Click);
             }
-            gButtom.Click += new System.EventHandler(this.gButtonClick);
+
+
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            int id = button1.TabIndex;
-            textBox1.AppendText(Convert.ToString(sender));
+            //int id = button1.TabIndex;
+            //textBox1.AppendText(Convert.ToString(sender));
+
             
             //this.btnAdd.Text = "Add";
             //this.btnAdd.Size = new System.Drawing.Size(25, 25);
@@ -42,13 +44,14 @@ namespace MineSweeper
             //this.btnAdd2.Size = new System.Drawing.Size(25, 25);
         }
 
-        public void gButtonClick(object sender, EventArgs e)
+        public void Button_Click(object sender, EventArgs e)
         {
 
             Button btn = sender as Button;
-            int id = gButtom.TabIndex;
-           //id = ((Button)sender).TabIndex;
-            
+            int id = btn.TabIndex;
+            //id = ((Button)sender).TabIndex;
+            MessageBox.Show("works");
+            MessageBox.Show(Convert.ToString(btn.TabIndex));
             textBox1.AppendText(Convert.ToString(id));
         }
     }
