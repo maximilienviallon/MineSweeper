2ebf0bdc9cad058530cd45a70ecbd753e76ebc02
2nowfake <baba_220@hotmail.com>
2019-08-30 04:52:11 +0200

    Ready for actual coding

--- MineSweeper/MineSweeper/Form1.cs
+++ MineSweeper/MineSweeper/Form1.cs
@@ -12,21 +12,30 @@ namespace MineSweeper
 {
     public partial class Form1 : Form
     {
-        private Button btnAdd = new Button();
+        //private Button btnAdd2 = new Button();
         public Form1()
         {
             InitializeComponent();
-
+            for (int i = 0; i < 5; i++)
+            {
+                Button gButtom = new Button();
+                Controls.Add(gButtom);
+                gButtom.Location = new System.Drawing.Point(90+25*i, 25);
+                gButtom.Size = new System.Drawing.Size(25, 25);
+                gButtom.BackColor = Color.LightGray;
+            }
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            this.Controls.Add(btnAdd);
-            this.btnAdd.Location = new System.Drawing.Point(90, 25);
-
-            this.btnAdd.BackColor = Color.Gray;
-            this.btnAdd.Text = "Add";
-            this.btnAdd.Size = new System.Drawing.Size(50, 25);
+            int id = button1.TabIndex;
+            textBox1.AppendText(Convert.ToString(id));
+            
+            //this.btnAdd.Text = "Add";
+            //this.btnAdd.Size = new System.Drawing.Size(25, 25);
+            //this.Controls.Add(btnAdd2);
+            //this.btnAdd2.Location = new System.Drawing.Point(115, 25);
+            //this.btnAdd2.Size = new System.Drawing.Size(25, 25);
         }
     }
 }
